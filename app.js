c8f5d536a70ae2c31f5e8405eb822a68c2d04cc0
rafaellafn <rafaferreiraneta@gmail.com>
2022-05-07 21:30:02 +0200

    Update JS

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 'use strict';
 
-/* <label class="todo__item"> 
-<input type="checkbox">
-<div>teste de item 1</div>
-<input type="button" value="X">
-</label> */
+let banco = [
+    {'tarefa':'Estudar JS', 'status':''},
+    {'tarefa':'netflix', 'status':'checked'},
+    {'tarefa':'teste1', 'status':'checked'}
+];
 
-const criarItem = (tarefa) => {
+const criarItem = (tarefa,status) => {
     const item = document.createElement('label');
     item.classList.add('todo__item');
     item.innerHTML=`
-        <input type="checkbox">
+        <input type="checkbox" ${status}>
         <div>${tarefa}</div>
         <input type="button" value="X">  
-    `
+    `;
     document.getElementById('todoList').appendChild(item);
 }
+
+const limparTarefas = () => {
+    const todoList = document.getElementById('todoList');
+    while(todoList.firstChild){
+        todoList.removeChild(todoList.lastChild);
+    }
+}
+
+const atualizarTela = () => {
+    limparTarefas()
+    banco.forEach (item => criarItem(item.tarefa, item.status));
+}
+
+atualizarTela();
+atualizarTela();
+atualizarTela();
+atualizarTela();
