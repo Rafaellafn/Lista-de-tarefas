410ab07bcfb9d39983d37f3d7e9ef09155f9bb69
rafaellafn <rafaferreiraneta@gmail.com>
2022-05-08 19:08:56 +0200

    Updat HTML e JS

--- app.js
+++ app.js
@@ -3,33 +3,48 @@
 let banco = [
     {'tarefa':'Estudar JS', 'status':''},
     {'tarefa':'netflix', 'status':'checked'},
-    {'tarefa':'teste1', 'status':'checked'}
+    {'tarefa':'teste1', 'status':''},
 ];
 
-const criarItem = (tarefa,status) => {
+const criarItem = (tarefa, status, indice) => {
     const item = document.createElement('label');
     item.classList.add('todo__item');
-    item.innerHTML=`
-        <input type="checkbox" ${status}>
+    item.innerHTML =`
+        <input type="checkbox" ${status} data-indice=${indice} >
         <div>${tarefa}</div>
-        <input type="button" value="X">  
+        <input type="button" value="X" data-indice=${indice}>  
     `;
     document.getElementById('todoList').appendChild(item);
 }
 
 const limparTarefas = () => {
     const todoList = document.getElementById('todoList');
-    while(todoList.firstChild){
-        todoList.removeChild(todoList.lastChild);
+    while (todoList.firstChild) {
+       todoList.removeChild(todoList.lastChild);
     }
 }
 
 const atualizarTela = () => {
-    limparTarefas()
-    banco.forEach (item => criarItem(item.tarefa, item.status));
+    limparTarefas();
+    banco.forEach (item, indice => criarItem(item.tarefa, item.status, indice));
 }
 
-atualizarTela();
-atualizarTela();
-atualizarTela();
+const inserirItem = (evento) => {
+    const tecla = evento.key;
+    const texto = evento.target.value;
+    if (tecla === 'Enter'){
+        banco.push ({'tarefa':texto, 'status':''});
+        atualizarTela();
+        evento.target.value = '';
+    }
+}
+
+const clickItem = (evento) => {
+    const elemento = evento.target;
+    console.log (elemento);
+}
+
+document.getElementById('newItem').addEventListener('keypress', inserirItem);
+document.getElementById('todoList').addEventListener('click', clickItem);
+
 atualizarTela();
